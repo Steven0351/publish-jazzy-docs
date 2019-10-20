18d50aa7ef38ddcd7df75c939644d0609542caf0
Steven Sherry <steven.sherry@affinityforapps.com>
2019-10-20 00:40:01 -0500

    Compile latest changes

--- dist/index.js
+++ dist/index.js
@@ -3737,7 +3737,7 @@ const generateAndDeploy = async () => {
   const jazzyDocs = getDocumentationFolder()
   await exec.exec("sudo gem install jazzy")
   await exec.exec(generateJazzyArguments())
-  await exec.exec(`ls -a | grep -v ${jazzyDocs} | xargs rm -rf`)
+  await exec.exec("ls -a | grep -v " + jazzyDocs + " | xargs rm -rf")
   await exec.exec(`cd ${jazzyDocs}`)
   
   const remote = `https://${token}@github.com/${context.repo.owner}/${context.repo.repo}.git`
