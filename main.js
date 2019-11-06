e07256afd516e67986c1079c19139445dd124cb5
Steven Sherry <steven.sherry@affinityforapps.com>
2019-11-05 22:50:00 -0600

    Add gitArguments function that returns an array of git commands to be executed by shell.exec based on the target branch. Fail the job if the branch is not gh-pages or master.

--- main.js
+++ main.js
@@ -6,17 +6,42 @@ const fs = require("fs")
 
 const context = github.context
 
-const gh_pagesBranch = "gh-pages"
+const pagesBranch = "gh-pages"
+const masterBranch = "master"
 
 // User defined input
 const jazzyVersion = core.getInput("version")
 const configFilePath = core.getInput("config")
 const jazzyArgs = core.getInput("args")
 const token = core.getInput("personal_access_token")
-const branch = core.getInput("branch") || gh_pagesBranch
+const branch = core.getInput("branch") || pagesBranch
 const message = core.getInput("message") || 'Deploying Updated Jazzy Docs'
 
 const remote = `https://${token}@github.com/${context.repo.owner}/${context.repo.repo}.git`
+
+const gitArguments = () => {
+  if (branch === pagesBranch) {
+    return [
+      "git init",
+      `git config user.name ${context.actor}`,
+      `git config user.email ${context.actor}@users.noreply.github.com`,
+      "git add .",
+      `git commit -m '${message}'`,
+      `git push --force ${remote} master:${branch}`
+    ]
+  }
+  
+  if (branch === masterBranch) {
+    return [
+      `git config --global user.name ${context.actor}`,
+      `git config --global user.email ${context.actor}@users.noreply.github.com`,
+      `git config --global user.password "${token}"`,
+      "git add .",
+      `git commit -m '${message}'`,
+      "git push"
+    ]
+  }
+}
 
 const generateJazzyInstallCommand = () => {
   let gemInstall = "sudo gem install jazzy"
@@ -83,22 +108,23 @@ const getDocumentationFolder = () => {
 }
 
 const generateAndDeploy = () => {
+  if (branch !== pagesBranch || branch !== masterBranch) {
+    core.setFailed
+  }
+
   shell.exec(generateJazzyInstallCommand())
   shell.exec(generateJazzyArguments())
 
-  if(branch == gh_pagesBranch) {
+  if(branch == pagesBranch) {
     shell.cp("-r", `${getDocumentationFolder()}/*`, "../")
 
     shell.cd("../")
     shell.rm("-rf", `${process.env.GITHUB_WORKSPACE}`)  
   }
 
-  shell.exec("git init")
-  shell.exec(`git config user.name ${context.actor}`)
-  shell.exec(`git config user.email ${context.actor}@users.noreply.github.com`)
-  shell.exec("git add .")
-  shell.exec(`git commit -m '${message}'`)
-  shell.exec(`git push --force ${remote} master:${branch}`)
+  gitArguments().forEach(command => {
+    shell.exec(command, { fatal: true })
+  })
 }
 
 try {
